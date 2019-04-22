d205d72ba8b3d9778900441a14c92c6803e119a4
huy <tranquanghuy097@gmail.com>
2019-04-22 18:56:39 +0700

    First

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+})
+
+app.get('/signin', function(req, res) {
+    res.render('signin');
+})
+ 
+app.listen(3000, function(){
+    console.log('service http://localhost:3000/ running')
+})
